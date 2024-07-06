e5dc06d390829a66e93f3d2a2e5774c145aa707c
cnorzen <thaibinh403h@gmail.com>
2024-07-06 17:53:43 +0700

    Code Task contact, blog, About

--- src/pages/Blog/BlogDetail.jsx
+++ src/pages/Blog/BlogDetail.jsx
@@ -1,30 +1,148 @@
-// import { useState } from "react";
-import { useParams } from "react-router-dom";
-import { Breadcrumb } from "antd";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import { Breadcrumb, Skeleton } from "antd";
+import "tailwindcss/tailwind.css";
+
 const BlogDetail = () => {
   const { id } = useParams();
-  console.log(id);
+  const [post, setPost] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const response = await axios.get(
+          `https://jsonplaceholder.typicode.com/posts/${id}`
+        );
+        setPost(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+        setLoading(false);
+      }
+    };
+
+    fetchPost();
+  }, [id]);
+
+  if (loading) {
+    return (
+      <div className="container mx-auto px-4 py-8">
+        <Skeleton active />
+      </div>
+    );
+  }
+
+  if (!post) {
+    return (
+      <div className="container mx-auto px-4 py-8">
+        <p className="text-center text-gray-600">Không tìm thấy bài đăng</p>
+      </div>
+    );
+  }
+
   return (
     <>
-      <div className="breadcrumb-site">
-        <Breadcrumb
-          items={[
-            {
-              title: <Link to="/">Home</Link>,
-            },
-            {
-              title: <Link to="/blog">Blog</Link>,
-            },
-            {
-              title: <Link to={`/blog/${id}`}>Blog {id}</Link>,
-            },
-          ]}
-        />
+      <div className="bg-white py-4 shadow-sm">
+        <div className="container mx-auto">
+          <Breadcrumb separator=">" className="text-gray-500">
+            <Breadcrumb.Item>
+              <Link
+                to="/"
+                className="text-sm text-gray-500 hover:text-gray-800 transition duration-300"
+              >
+                Home
+              </Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>
+              <Link
+                to="/blog"
+                className="text-sm text-gray-500 hover:text-gray-800 transition duration-300"
+              >
+                Blog
+              </Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>{post.title}</Breadcrumb.Item>
+          </Breadcrumb>
+        </div>
       </div>
-      <div className="blog-detail-site">
-        <h1 className="text-3xl">Blog Detail</h1>
-        <p className="">Blog Detail: {id}</p>
+      <div className="container mx-auto px-4 py-12">
+        <div className="max-w-4xl mx-auto bg-white rounded-lg  overflow-hidden">
+          <div className="relative">
+            <img
+              src={`https://picsum.photos/800/400?random=${post.id}`}
+              alt={post.title}
+              className="w-full h-96 object-cover"
+            />
+            <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black to-transparent p-6">
+              <h1 className="text-4xl font-bold text-white">{post.title}</h1>
+            </div>
+          </div>
+          <div className="p-8">
+            <div className="prose prose-lg max-w-none text-gray-800 leading-relaxed">
+              <p>{post.body}</p>
+              <br />
+              <p>
+                Không còn đơn thuần là nguồn cảm hứng cho thiết kế, nghệ thuật
+                đã trở thành lĩnh vực đầu tư có chiến lược của những tập đoàn
+                dẫn đầu trong ngành thời trang xa xỉ. Tuy là hai lĩnh vực tách
+                biệt nhưng giữa thời trang và nghệ thuật vẫn luôn tồn tại sự
+                liên kết mật thiết. Sự giao thoa đó ngày một rõ nét khi nhiều
+                thương hiệu, tập đoàn thể hiện sự quan tâm và đầu tư nghiêm túc
+                vào nghệ thuật. Điều này không khó hiểu bởi trong thời đại ngày
+                nay, mục tiêu của ngành hàng xa xỉ không chỉ dừng lại ở việc tạo
+                ra sản phẩm chất lượng đỉnh cao mà hơn thế là tập trung vào lối
+                sống của giới thượng lưu, tạo những kết nối ý nghĩa, và ở đó,
+                nghệ thuật là một yếu tố quan trọng. Xu hướng mở rộng sang lĩnh
+                vực nghệ thuật bắt đầu từ sở thích cá nhân của giới tinh hoa,
+                những nhà sáng lập của các thương hiệu và tập đoàn lớn, trong đó
+                có thể kể đến Miuccia Prada, François Pinault, Bernard Arnault…
+              </p>
+              <br />
+
+              <p>
+                Ngoài cặp mắt sắc bén trong thiết kế, Miuccia Prada còn là người
+                có tầm nhìn đầy tri thức và đam mê nghệ thuật. Điều đó góp phần
+                tạo nên vẻ đẹp tinh vi đa tầng trong hai thương hiệu của bà –
+                Prada và Miu Miu. Với tầm nhìn tiên phong, bà và chồng là doanh
+                nhân Patrizio Bertelli đã sáng lập viện Fondazione Prada vào năm
+                1993. Đây là dự án kiến trúc đầu tiên của giới thời trang được
+                thiết kế bởi một văn phòng kiến trúc nổi tiếng, cụ thể là OMA do
+                KTS Rem Koolhaas chủ trì. Kể từ đó, khu phức hợp này đã trở
+                thành không gian văn hóa nghệ thuật đương đại, đồng thời là nơi
+                diễn ra các show diễn của thương hiệu. Chưa dừng lại ở đó, tập
+                đoàn còn giới thiệu Prada Frame, một hội nghị chuyên đề đa ngành
+                đào sâu vào mối quan hệ phức tạp giữa môi trường tự nhiên và
+                thiết kế bắt đầu từ năm 2022, diễn ra trong khuôn khổ Milan
+                Fashion Week thường niên.
+              </p>
+              <br />
+              <p>
+                Kể từ sau Fondazione Prada, các doanh nhân thời trang cũng bắt
+                đầu thể hiện sự quan tâm nghệ thuật của mình một cách rộng rãi
+                hơn thay vì lưu trữ chúng cho riêng mình. Năm 1999, tỉ phú
+                François Pinault, nhà sáng lập tập đoàn Kering thành lập Pinault
+                Collection nhằm quản lý các BST nghệ thuật tư nhân của gia tộc
+                Pinault, địa điểm triển lãm, chương trình liên kết từ thiện và
+                văn hóa… Tính đến nay, ông đã sở hữu ba công trình cổ tại Venice
+                và được cấp quyền sử dụng 50 năm cho một tòa nhà di sản tại
+                Paris là Bourse de Commerce để phục vụ mục đích nghệ thuật. Cả
+                bốn công trình Palazzo Grassi, Punta della Dogana, Teatrino và
+                gần nhất là Bourse de Commerce đều được cải tạo bởi KTS đoạt
+                giải Pritzker người Nhật Tadao Ando.
+              </p>
+            </div>
+            <div className="mt-8">
+              <Link
+                to="/blog"
+                className="text-blue-600 hover:text-blue-800 transition duration-300 text-lg"
+              >
+                &larr; Quay lại blog
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,19 @@
 import { Breadcrumb, Button, Form, Input } from "antd";
 import TextArea from "antd/es/input/TextArea";
 import { Link } from "react-router-dom";
+import { PhoneOutlined, MailOutlined, HomeOutlined } from "@ant-design/icons";
+
 const onFinish = (values) => {
   console.log("Success:", values);
 };
+
 const onFinishFailed = (errorInfo) => {
   console.log("Failed:", errorInfo);
 };
+
 const ContactPage = () => (
   <>
-    <div className="breadcrumb-site container mx-auto py-2">
+    <div className="breadcrumb-site container mx-auto py-4">
       <Breadcrumb
         items={[
           {
@@ -22,17 +26,17 @@ const ContactPage = () => (
       />
     </div>
     <div className="container mx-auto my-20">
-      <h1 className="title my-8">Contact</h1>
-      <div className="grid grid-cols-2">
-        <div className="form-contact">
+      <h1 className="text-6xl font-bold text-center my-12 text-gray-900">
+        Contact Us
+      </h1>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-16">
+        <div className="lg:col-span-2 bg-gray-50 p-12 shadow-xl rounded-2xl">
+          <h2 className="text-3xl font-semibold mb-8 text-gray-700">
+            Emma có thể giúp gì cho bạn ?
+          </h2>
           <Form
             name="basic"
-            labelCol={{
-              span: 4,
-            }}
-            wrapperCol={{
-              span: 16,
-            }}
+            layout="vertical"
             initialValues={{
               remember: true,
             }}
@@ -45,11 +49,14 @@ const ContactPage = () => (
               rules={[
                 {
                   required: true,
-                  message: "Please input your username!",
+                  message: "Please input your name!",
                 },
               ]}
             >
-              <Input placeholder="Your Name" />
+              <Input
+                placeholder="Your Name"
+                className="p-4 border-2 border-gray-200 rounded-lg"
+              />
             </Form.Item>
 
             <Form.Item
@@ -59,46 +66,119 @@ const ContactPage = () => (
                   required: true,
                   message: "Please input your email!",
                 },
-              ]}
-            >
-              <Input placeholder="Your Email" />
-            </Form.Item>
-            <Form.Item
-              name="avia_3_1"
-              rules={[
                 {
-                  required: true,
-                  message: "Please input your comment!",
+                  type: "email",
+                  message: "The input is not valid E-mail!",
                 },
               ]}
             >
-              <TextArea placeholder="Comment" rows={4} />
+              <Input
+                placeholder="Your Email"
+                className="p-4 border-2 border-gray-200 rounded-lg"
+              />
             </Form.Item>
             <Form.Item
-              wrapperCol={{
-                span: 16,
-              }}
+              name="comment"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your message!",
+                },
+              ]}
             >
-              <Button className="w-full" type="primary" htmlType="submit">
-                Submit
+              <TextArea
+                placeholder="Your Message"
+                rows={4}
+                className="p-4 border-2 border-gray-200 rounded-lg"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Button
+                className="w-full p-0 text-lg bg-blue text-white hover:bg-gray-800 transition-colors duration-300"
+                type="primary"
+                htmlType="submit"
+              >
+                Gửi lời nhắn
               </Button>
             </Form.Item>
           </Form>
         </div>
 
-        <div className="map">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.7257653295324!2d105.779069212123!3d21.043656080528475!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454ccae536be7%3A0x509333dd21815f6c!2zUGjhuqFtIFbEg24gxJDhu5NuZywgTWFpIEThu4tjaCwgQ-G6p3UgR2nhuqV5LCBIw6AgTuG7mWksIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1717933166866!5m2!1svi!2s"
-            width={600}
-            height={450}
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          />
+        <div className="bg-gray-50 p-12 shadow-xl rounded-2xl">
+          <h2 className="text-3xl font-semibold mb-6 text-gray-700">
+          Liên Hệ Chúng Tôi
+          </h2>
+          <p className="text-lg mb-4">
+          Hãy liên hệ với chúng tôi để có được sự hỗ trợ
+          </p>
+          <div className="flex items-center mb-4">
+            <PhoneOutlined className="text-2xl mr-4 text-gray-600" />
+            <span className="text-lg text-gray-700">0912345678</span>
+          </div>
+          <br />
+          <div className="flex items-center mb-4">
+            <MailOutlined className="text-2xl mr-4 text-gray-600" />
+            <span className="text-lg text-gray-700">
+              support@Emmafashion.com
+            </span>
+          </div>
+          <br />
+          <div className="flex items-center">
+            <HomeOutlined className="text-2xl mr-4 text-gray-600" />
+            <span className="text-lg text-gray-700">
+              FPT Polytechnic Building, Trinh Van Bo Street, Phuong Canh Ward, Nam Tu Liem District, Hanoi City
+            </span>
+          </div>
         </div>
       </div>
+
+      <section className="bg-gray-50 mt-16 py-16">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="max-w-2xl lg:max-w-4xl mx-auto text-center">
+            <h2 className="text-4xl font-extrabold text-gray-900">
+            Ghé Thăm địa điểm cửa hàng của Emma
+            </h2>
+            <p className="mt-4 text-lg text-gray-600">
+            Hãy khám phá các địa điểm cửa hàng của chúng tôi và tìm địa điểm gần bạn nhất.
+            </p>
+          </div>
+          <div className="mt-16 lg:mt-20 grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div className="rounded-lg overflow-hidden shadow-lg">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.861924320462!2d105.7402342151451!3d21.036894192863796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3134533c78c6c8a3%3A0xc808f34a3066c3b7!2sFPT%20Polytechnic%20H%C3%A0%20N%E1%BB%99i!5e0!3m2!1sen!2s!4v1624275977595!5m2!1sen!2s"
+                width="100%"
+                height="480"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+              ></iframe>
+            </div>
+            <div className="bg-white rounded-lg overflow-hidden shadow-lg p-6">
+              <h3 className="text-xl font-medium text-gray-900">
+                Địa chỉ cửa hàng Emma
+              </h3>
+              <p className="mt-2 text-gray-600">
+                FPT Polytechnic, Trinh Van Bo Street, Hanoi, Vietnam
+              </p>
+              <div className="border-t border-gray-200 mt-4 pt-4">
+                <h3 className="text-xl font-medium text-gray-900">Lịch mở cửa</h3>
+                <p className="mt-2 text-gray-600">Thứ Hai- Thứ Năm: 9am - 5pm</p>
+                <p className="mt-1 text-gray-600">Thứ Bảy: 9am - 5pm</p>
+                <p className="mt-1 text-gray-600">Chủ nhật: Đóng cửa</p>
+              </div>
+              <div className="border-t border-gray-200 mt-4 pt-4">
+                <h3 className="text-xl font-medium text-gray-900">Liên Hệ</h3>
+                <p className="mt-2 text-gray-600">
+                  Email: support@Emmafashion.com
+                </p>
+                <p className="mt-1 text-gray-600">Số điện thoại: 0912345678</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   </>
 );
+
 export default ContactPage;
