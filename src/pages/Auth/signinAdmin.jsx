4a1ada5ab2b6391c6abd9800ea0831bf2a475a17
cnorzen <thaibinh403h@gmail.com>
2024-08-22 02:52:31 +0700

    update Staff

--- src/pages/Auth/signinAdmin.jsx
+++ src/pages/Auth/signinAdmin.jsx
@@ -1,25 +1,29 @@
-
-import { useState } from 'react';
-import { Button, Form, Input, message } from 'antd';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { Button, Form, Input, message } from "antd";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const SigninAdmin = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
 
   const loginUser = async (formData) => {
     setLoading(true);
-    setErrorMessage('');
+    setErrorMessage("");
     try {
-      const response = await axios.post('http://localhost:8000/api/login', formData);
-      localStorage.setItem('token', response.data.token);
-      message.success('Đăng nhập thành công!');
-      navigate('/admin');
+      const response = await axios.post(
+        "http://localhost:8000/api/login",
+        formData
+      );
+      localStorage.setItem("token", response.data.token);
+      message.success("Đăng nhập thành công!");
+      navigate("/admin");
     } catch (error) {
       console.error("Login failed:", error);
-      setErrorMessage("Đăng nhập không thành công. Vui lòng kiểm tra lại email hoặc mật khẩu.");
+      setErrorMessage(
+        "Đăng nhập không thành công. Vui lòng kiểm tra lại email hoặc mật khẩu."
+      );
     } finally {
       setLoading(false);
     }
@@ -41,9 +45,7 @@ const SigninAdmin = () => {
           Đăng nhập
         </h1>
         {errorMessage && (
-          <div className="mb-4 text-red-600 text-center">
-            {errorMessage}
-          </div>
+          <div className="mb-4 text-red-600 text-center">{errorMessage}</div>
         )}
         <Form
           name="signin"
@@ -71,9 +73,7 @@ const SigninAdmin = () => {
             name="password"
             rules={[{ required: true, message: "Vui lòng nhập mật khẩu!" }]}
           >
-            <Input.Password
-              className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
+            <Input.Password className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
           </Form.Item>
 
           <Form.Item
