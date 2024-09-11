c897c32a92dbe67e623ca93f35218429b37c5145
longnq264 <longhb2642002@gmail.com>
2024-09-11 14:23:14 +0700

    conflix

--- src/App.jsx
+++ src/App.jsx
@@ -51,25 +51,12 @@ import AttributeList from "./components/User/Attributes/AttributeList.jsx";
 import EditAttribute from "./components/User/Attributes/EditAttribute.jsx";
 import AddAttribute from "./components/User/Attributes/AddAttribute.jsx";
 import ProfileAdmin from "./pages/Admin/ProfileAdmin.jsx";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import ProductListDetail from "./pages/Admin/ProductListDetail.jsx";
 
-=======
-=======
->>>>>>> 198108a (merge)
 import CouponList from "./components/User/Coupon/CouponList.jsx";
 import CouponAdd from "./components/User/Coupon/CouponAdd.jsx";
 import CouponDetail from "./components/User/Coupon/CouponDetail.jsx";
 import CouponEdit from "./components/User/Coupon/CouponEdit.jsx";
-<<<<<<< HEAD
->>>>>>> hieu/Coupon
-=======
-=======
-import ProductListDetail from "./pages/Admin/ProductListDetail.jsx";
-
->>>>>>> 4c6152f (update Detail Product List  Admin)
->>>>>>> 198108a (merge)
 
 function App() {
   const router = createBrowserRouter([
@@ -137,25 +124,12 @@ function App() {
         { path: "attributes", element: <AttributeList /> },
         { path: "attributes/edit/:id", element: <EditAttribute /> },
         { path: "attributes/new", element: <AddAttribute /> },
-<<<<<<< HEAD
-<<<<<<< HEAD
         { path: "products/:id", element: <ProductListDetail /> },
 
-=======
-=======
->>>>>>> 198108a (merge)
         { path: "coupons", element: <CouponList /> },
         { path: "coupons/new", element: <CouponAdd /> },
         { path: "coupons/edit/:id", element: <CouponEdit /> },
-        { path: "coupons/:id", element: <CouponDetail  /> },
-<<<<<<< HEAD
->>>>>>> hieu/Coupon
-=======
-=======
-        { path: "products/:id", element: <ProductListDetail /> },
-
->>>>>>> 4c6152f (update Detail Product List  Admin)
->>>>>>> 198108a (merge)
+        { path: "coupons/:id", element: <CouponDetail /> },
       ],
     },
     {
