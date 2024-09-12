0ba4e9f2d4904a2d061ad54f0da897e4e481eb95
LuuQuyToan <luutoan0356@gmail.com>
2024-09-12 23:46:37 +0700

    update forgot password

--- src/pages/Auth/ForgotPassword.jsx
+++ src/pages/Auth/ForgotPassword.jsx
@@ -1,58 +1,49 @@
-import { Button, Form, Input } from "antd";
-
+import { useState } from 'react'
+import axios from "axios";
+import { message } from 'antd'
 const ForgotPassword = () => {
-  const onFinish = async (values) => {
-    console.log(values);
-  };
-  const onFinishFailed = async (values) => {
-    console.log(values);
-  };
+
+  const [email, setEmail] = useState('')
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/api/forgetpassword', {
+        email: email
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+      message.success(response.data.message)
+    } catch (error) {
+      message.error(error.response.data.data.email)
+    }
+  }
   return (
     <div className="mt-20">
       <div className="container mx-auto">
         <div className="wrap-content flex justify-center">
-          <div className="bg-white bg-opacity-20 w-96 p-10 rounded-r-lg shadow-2xl">
+          <div className="bg-white bg-opacity-20 w-96 p-10 rounded-lg shadow-2xl">
             <h1 className="font-bold text-center text-white text-3xl mb-10 ">
-              ForgotPass
+              Forgot Password
             </h1>
-            <Form
-              name="basic"
-              initialValues={{
-                remember: true,
-              }}
-              onFinish={onFinish}
-              onFinishFailed={onFinishFailed}
-              autoComplete="off"
-              className=""
-            >
-              <Form.Item
-                name="email"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your username!",
-                  },
-                ]}
-                className="mb-4 text-sm font-medium text-gray-700"
-              >
-                <Input
-                  className="bg-white bg-opacity-20 border-none h-10"
-                  placeholder="Email"
+            <form action="" className="w-full">
+              <div>
+                <input
+                  name="email"
+                  type="text"
+                  className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md 
+                            text-sm shadow-sm placeholder-slate-400
+                            focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                            invalid:border-pink-500 invalid:text-pink-600
+                            focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+                  placeholder="Nhập email"
+                  onChange={(e) => { setEmail(e.target.value) }}
                 />
-              </Form.Item>
-
-              <Form.Item className="">
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  loading={status === "loading"}
-                  className="w-full bg-orange-400 bg-opacity-50 h-10"
-                >
-                  <p className="font-bold">Send Mail</p>
-                </Button>
-              </Form.Item>
-              {/* {error && <div>Error</div>} */}
-            </Form>
+                <button type="button" onClick={handleSubmit} className="w-full bg-orange-400 bg-opacity-50 p-1 mt-2 rounded-md text-white hover:bg-orange-500">
+                  Submit
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
---
+++ src/pages/Auth/ResetPassword.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { message } from 'antd'
+import axios from "axios";
+
+
+const ResetPassword = () => {
+    const location = useLocation();
+    const navigate = useNavigate()
+
+    const queryParams = new URLSearchParams(location.search);
+    const token = queryParams.get('token');
+    const email = queryParams.get('email');
+
+    const [password, setPasswrod] = useState('')
+    const [rePassword, setRePasswrod] = useState('')
+
+    const handleSubmit = async () => {
+        try {
+            const response = await axios.post('http://127.0.0.1:8000/api/resetpassword', {
+                token: token,
+                email: email,
+                password: password,
+                re_password: rePassword
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            })
+            message.success(response.data.message)
+            navigate('/auth/login')
+        } catch (error) {
+            message.error(error.response.data.data.password ?? error.response.data.data.re_password)
+        }
+    }
+
+    return (
+        <div className="mt-20">
+            <div className="container mx-auto">
+                <div className="wrap-content flex justify-center">
+                    <div className="bg-white bg-opacity-20 w-96 p-10 rounded-lg shadow-2xl">
+                        <h1 className="font-bold text-center text-white text-3xl mb-10 ">
+                            Change Password
+                        </h1>
+                        <form action="" className="w-full">
+                            <div>
+                                <input
+                                    type="password"
+                                    className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md 
+                            text-sm shadow-sm placeholder-slate-400
+                            focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                            invalid:border-pink-500 invalid:text-pink-600
+                            focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+                                    placeholder="Nhập mật khẩu mới"
+                                    onChange={(e) => { setPasswrod(e.target.value) }}
+                                />
+                                <input
+                                    type="password"
+                                    className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md 
+                            text-sm shadow-sm placeholder-slate-400
+                            focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                            invalid:border-pink-500 invalid:text-pink-600
+                            focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+                                    placeholder="Nhập lại mật khẩu mới"
+                                    onChange={(e) => { setRePasswrod(e.target.value) }}
+                                />
+                                <button type="button" onClick={handleSubmit} className="w-full bg-orange-400 bg-opacity-50 p-1 mt-2 rounded-md text-white hover:bg-orange-500">
+                                    Submit
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ResetPassword
